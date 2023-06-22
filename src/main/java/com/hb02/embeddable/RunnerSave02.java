c9125155cf27e7e8d06b1be85a479aea29ee423e
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-06-22 11:01:59 +0200

    RunnerSave

---
+++ src/main/java/com/hb02/embeddable/RunnerSave02.java
@@ -0,0 +1,45 @@
+package com.hb02.embeddable;
+
+import com.hb01.annotations.Student01;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerSave02 {
+
+    public static void main(String[] args) {
+
+        Student02 student1 = new Student02();
+        student1.setId(1);
+        student1.setName("Jace");
+        student1.setGrade(90);
+
+        Address address = new Address("Apple Street", "New York", "USA", "01-001");
+        student1.setAddress(address);
+
+
+        Student02 student2 = new Student02();
+        student2.setId(2);
+        student2.setName("John");
+        student2.setGrade(90);
+        student2.setAddress(new Address("Queens", "New York", "USA", "01-001"));
+
+        Configuration con = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student02.class);
+
+        SessionFactory sessionFactory = con.buildSessionFactory();  //create session
+        Session session = sessionFactory.openSession();     //create our room
+        Transaction transaction = session.beginTransaction();
+
+
+        session.save(student1);
+        session.save(student2);
+
+
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
+    }
+}
