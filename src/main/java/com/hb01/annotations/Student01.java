ba6f7e684830242d7f4ca87e71a7c4469a0d1d9d
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-06-25 16:08:19 +0200

    Student01

--- src/main/java/com/hb01/annotations/Student01.java
+++ src/main/java/com/hb01/annotations/Student01.java
@@ -5,19 +5,24 @@ import java.util.Arrays;
 
 //Annotation are like interfaces  -->  public @interface Entity
 
-@Entity     //With Entity, we are creating a table in our database
-@Table(name = "t_student01")
-public class Student01 {
 
-    @Id    //Defining a Primary key - Necessary
-    private int id;                     //in pojo class access modifier of variables are private
+@Entity                        //this class will be mapped to table. With Entity, we are creating a table in our database
+@Table(name = "t_student01")   //to customize table name, we use it for rename table name
+public class Student01 {       //default name of the table will be "Student01"
+
+    @Id                        //The field under @Id will be Primary key. Necessary
+    private int id;            //in pojo class access modifier of variables are private
+
+
 
     //@Column is optional
     @Column(name="student_name", length = 50, nullable = false, unique = false)
-    private String name;            //it is enough to create a acolumn which name is "name"
+    private String name;            //by default variable name will be the column name
                                     //nullable = false --> I should write something
 
-   //@Transient      //making it not appear on the DB, as a column
+
+
+   //@Transient      //making it not appear on the DB, as a column, this field will not be added to the table
     //@id  if we want to make grade primary key, we have to write @id above it
     private int grade;
 
@@ -52,6 +57,7 @@ public class Student01 {
     public void setName(String name) {
         this.name = name;
     }
+
 
     public int getGrade() {
         return grade;
