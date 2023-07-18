e324759f9bd111a7db2909fcf58d235d0c5e4348
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-07-18 18:25:41 +0200

    RunnerFetch08

---
+++ src/main/java/com/hb08/manytomany/RunnerFetch08.java
@@ -0,0 +1,42 @@
+package com.hb08.manytomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerFetch08 {
+
+    public static void main(String[] args) {
+
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Student08.class).
+                addAnnotatedClass(Book08.class);
+        SessionFactory sessionFactory = con.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+
+
+        //get student by id   with get method
+        Student08 student1 = session.get(Student08.class,1001L);
+        System.out.println(student1);
+
+//        System.out.println(student1.getBookList());
+
+
+        //get book by id with get method
+        Book08 book1=  session.get(Book08.class,103L);
+        System.out.println(book1);
+//        System.out.println(book1.getStudents());
+
+
+
+
+
+
+
+        tx.commit();
+        session.close();
+        sessionFactory.close();
+    }
+}
